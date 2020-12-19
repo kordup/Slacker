27cdb397b5e8c952357a24d53839dcebd8fee731
korrupt <akefaay@gmail.com>
2020-12-18 19:40:12 -0500

    V.5 Final

--- nmap.py
+++ nmap.py
@@ -23,53 +23,33 @@ def menu():
     global target
     mi = print("Current Target: [" + BC.F + target + BC.G + "]")
     mi = print("Custom Arguments: [" + BC.F + customArgs + BC.G + "]")
-    mitems("Set Target", "Quick Port Scan", "Deep Port Scan", "System Info Scan", "Known Vuln Scan", "Run With Just Custom Arguments", "Custom Arguments", "Clear Arguments")
+    mitems("Quick Port Scan", "Deep Port Scan", "System Info Scan", "Known Vuln Scan", "Run With Just Custom Arguments")
     mi = input("")
     mp = mi[:7]
     mo = mi[8:]
     np = mi[:5]
     no = mi[6:]
     na = mi[6:]
-    if mi == "1":
-        clear()
-        target = input("Enter A Target: ")
-        with open('nmap/target.py', 'w') as f:
-            f.write(target)
-        clear()
-        menu()
-    elif mi == "2":
+    elif mi == "1":
         clear()
         os.system('nmap --top-ports 20 ' + target + customArgs)
         menu()
         #clear()
-    elif mi == "3":
+    elif mi == "2":
         clear()
         os.system('sudo nmap -v -sS -sV ' + target + customArgs)
         menu()
-    elif mi == "4":
+    elif mi == "3":
         clear()
         os.system('sudo nmap -O ' + target + customArgs)
         menu()
-    elif mi == "5":
+    elif mi == "4":
         clear()
         os.system('nmap -Pn --script vuln ' + target + customArgs)
         menu()
-    elif mi == "6":
+    elif mi == "5":
         clear()
         os.system('sudo nmap ' + target + ' ' + customArgs)
-        menu()
-    elif mi == "7":
-        os.system('nmap -h')
-        customArgs = input("Enter Arguments: ")
-        with open('nmap/customArgs.py', 'w') as f:
-            f.write(' ' + customArgs)
-        clear()
-        menu()
-    elif mi == "8":
-        with open('nmap/customArgs.py', 'w') as f:
-            f.write('')
-        clear()
-        customArgs = ""
         menu()
     elif mi == "!":
         vulnscans.menu()
